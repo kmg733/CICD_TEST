3173bb5d265748da024928afda5e8bc4c2d5b084
kmg733 <kmg733@jiran.com>
2023-02-23 16:28:10 +0900

    Initial develop branch

---
+++ src/main/java/com/kmg733/cicdTest/CicdTestApplication.java
@@ -0,0 +1,17 @@
+package com.kmg733.cicdTest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class CicdTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CicdTestApplication.class, args);
+	}
+
+}
